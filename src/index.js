44c9ddeef69511d4948f56b327d5d8f598c52a5a
Bart Veneman <bartveneman@users.noreply.github.com>
2019-09-05 23:05:59 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-// const core = require("@actions/core");
-
 function run() {
-  console.log("Started `run()`");
-  // const pwToken = core.getInput("project_wallace_token");
-  const pwToken = process.env.INPUT_PROJECT_WALLACE_TOKEN;
+  const pwToken = process.env.INPUT_PROJECT_WALLACE_WEBHOOK_TOKEN;
   console.log("pwToken", pwToken);
-  // core.debug(`pwToken: ${pwToken}`);
-  // core.setOutput(
-  //   "css_analytics_diff",
-  // JSON.stringify({
-  //   "stylesheets.size": {
-  //     oldValue: 1,
-  //     newValue: 2,
-  //     diff: 2
-  //   }
-  // })
-  // );
-  console.log("ended `run()`");
-  // issueCommand('set-output', {name}, value)
   process.stdout.write(
     `##[set-output name=css_analytics_diff;]${JSON.stringify({
       "stylesheets.size": {
